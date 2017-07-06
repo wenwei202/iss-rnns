90653a348ca854b0e68a77b899b7c24ac84433e3
Wei Wen <weiwen.web@gmail.com>
2017-07-06 16:28:42 -0400

    groups with different hidden_size

--- test.sh
+++ test.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-export OUTPUT_DIR=$1
+if [ "$#" -lt 3 ]; then
+	echo "Illegal number of parameters"
+	echo "Usage: $0 OUTPUT_DIR HIDDEN_SIZE GROUP_CONFIG"
+	exit
+fi
+
+
+OUTPUT_DIR=$1
+HIDDEN_SIZE=$2
+GROUP_CONFIG=$3
 
 python -m basic.cli --len_opt --cluster \
 --shared_path ${OUTPUT_DIR}/basic/00/shared.json \
 --load_path ${OUTPUT_DIR}/basic/00/save/basic-10000 \
 --num_gpus 2 --batch_size 30 \
+--hidden_size ${HIDDEN_SIZE} \
 --plot_weights \
---group_config groups.json
+--group_config ${GROUP_CONFIG}
 
 python squad/evaluate-v1.1.py \
 $HOME/data/squad/dev-v1.1.json out/basic/00/answer/test-000000.json
