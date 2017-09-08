5e694b241b1df5df01e5e9e0c4d5fcb59ccd0272
Wei Wen <weiwen.web@gmail.com>
2017-09-08 12:06:19 -0400

    upgrade to python 3

--- ptb/reader.py
+++ ptb/reader.py
@@ -27,7 +27,8 @@ import tensorflow as tf
 
 def _read_words(filename):
   with tf.gfile.GFile(filename, "r") as f:
-    return f.read().decode("utf-8").replace("\n", "<eos>").split()
+    #return f.read().decode("utf-8").replace("\n", "<eos>").split()
+    return f.read().replace("\n", "<eos>").split()
 
 
 def _build_vocab(filename):
